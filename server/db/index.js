4ce2e40388f7f58374b979ec61c8ac80286757f4
Jeff Williams <88175879+orphandeity@users.noreply.github.com>
2023-09-26 09:53:12 -0700

    refactors db singleton

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,27 @@
 const { Pool } = require("pg");
 const { DB } = require("../config");
 
-const pool = new Pool({
-  user: DB.PGUSER,
-  host: DB.PGHOST,
-  database: DB.PGDATABASE,
-  password: DB.PGPASSWORD,
-  port: DB.PGPORT,
-});
+let instance = null;
 
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
+class Database {
+  constructor() {
+    if (!instance) {
+      instance = this;
+      this.pool = new Pool({
+        user: DB.PGUSER,
+        host: DB.PGHOST,
+        database: DB.PGDATABASE,
+        password: DB.PGPASSWORD,
+        port: DB.PGPORT,
+      });
+    }
+
+    return instance;
+  }
+
+  query(text, params) {
+    return this.pool.query(text, params);
+  }
+}
+
+module.exports = new Database();
