ae3ba987e55140dc737c0fe8d4add6fccbcfac07
Jeff Williams <88175879+orphandeity@users.noreply.github.com>
2023-09-25 22:34:05 -0700

    initial commit

---
+++ server/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  PORT: process.env.PORT,
+  SESSION_SECRET: process.env.SESSION_SECRET,
+  DB: {
+    PGHOST: process.env.PGHOST,
+    PGUSER: process.env.PGUSER,
+    PGDATABASE: process.env.PGDATABASE,
+    PGPASSWORD: process.env.PGPASSWORD,
+    PGPORT: process.env.PGPORT,
+  },
+};
---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const helmet = require("helmet");
+const session = require("express-session");
+const app = express();
+
+const { PORT, SESSION_SECRET } = require("./config");
+
+// Middleware
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false, maxAge: 1000 * 60 * 60 * 24 },
+  })
+);
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+// Errors
+app.use((err, req, res, next) => {
+  const { message, status } = err;
+  return res.status(status).json({ message });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}!`);
+});
