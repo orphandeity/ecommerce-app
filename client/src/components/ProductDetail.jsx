2d59b80e6e24863159e391bdb8a6459117c224e9
Jeff Williams <88175879+orphandeity@users.noreply.github.com>
2023-09-29 22:59:25 -0700

    adds ProductDetail grid styles

--- client/src/components/ProductDetail.jsx
+++ client/src/components/ProductDetail.jsx
@@ -3,10 +3,17 @@ import PropTypes from "prop-types";
 export default function ProductDetail({ product }) {
   return (
     <article>
-      <h2>{product.name}</h2>
-      <img src="https://www.picsum.photos/800/450" alt="" />
-      <p>{product.description}</p>
-      <p>{product.price_usd}</p>
+      <div
+        style={{ display: "grid", gridTemplateColumns: "2fr 1fr", gap: "1rem" }}
+      >
+        <img src="https://www.picsum.photos/1200/800" alt="" />
+        <div>
+          <h2>{product.name}</h2>
+          <p>{product.description}</p>
+          <p>{product.price_usd}</p>
+          <button>Add to cart</button>
+        </div>
+      </div>
     </article>
   );
 }
